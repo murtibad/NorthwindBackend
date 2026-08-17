7918b0b2e0c964b1ef316961adf536a3e9e12777
murat <murat.16-16@hotmail.com>
2026-08-17 16:48:20 +0300

    feat: Add generic repository interfaces and NorthwindContext

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class, IEntity,new()
+    {
+        T Get(Expression<Func<T, bool>>? filter=null);
+        IList<T> GetList();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    internal class NorthwindContext
+    {
+    }
+}
