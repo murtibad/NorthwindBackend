94d7fe11a04b77357a6e97a1d9d852b7e324fd10
murat <murat.16-16@hotmail.com>
2026-08-20 12:43:39 +0300

    feat: Add role management entities and DataAccess configuration
    Created User, OperationClaim, and UserOperationClaim entities in Core.Entities
    
    Added IUserDal and implemented EfUserDal with GetClaims LINQ query
    
    Updated NorthwindContext with new DbSet tables

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,12 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        List<OperationClaim> GetClaims(User user);
+    }
+}
