81130fcaf1ee4fe9266871aabc20b9b11a0b918d
murat <murat.16-16@hotmail.com>
2026-08-21 16:51:57 +0300

    feat: Add TokenOptions, SecurityKeyHelper and JwtHelper skeleton
    Added TokenOptions configuration model
    
    Created SecurityKeyHelper for symmetric security key generation
    
    Implemented JwtHelper base setup with IConfiguration injection

---
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Core.Entities.Concrete;
+using Core.Utilities.Security.Encryption;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+        }
+
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            return null; // daha sonra gerçek token döndürülecek
+        }
+    }
+}
