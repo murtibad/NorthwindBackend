48745d06da776a010e6d6dfe98379aa43ea08201
murat <murat.16-16@hotmail.com>
2026-08-24 16:57:52 +0300

    feat: implement CreateAccessToken method
    - Implemented CreateAccessToken method in AuthManager
    - Added UserRegistered and AccessTokenCreated constants to Messages

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@
         public static string PasswordError = "Şifre hatalı";
         public static string SuccessfulLogin = "Sisteme giriş başarılı";
         public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
     }
 }
